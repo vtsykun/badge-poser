34e2c5ea7bda018b1db438082aaeb39e2a1b4f9e
Vladimir Tsykun <vtsykun@okvpn.org>
2018-02-20 00:19:42 +0300

    Remove GA script Disable bootstrap.php.cache

--- web/app.php
+++ web/app.php
@@ -1,17 +1,13 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-$loader = new ApcClassLoader('sf2', $loader);
-$loader->register(true);
+$loader = require_once __DIR__.'/../app/autoload.php';
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
